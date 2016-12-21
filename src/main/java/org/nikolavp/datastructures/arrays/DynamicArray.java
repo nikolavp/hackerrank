8a2ef3cdc230a9fbae0b338b0151916945a72fbf
Nikola Petrov <nikolavp@gmail.com>
2016-12-21 17:12:38 +0200

    Dynamic arrays challange

--- src/main/java/org/nikolavp/datastructures/arrays/DynamicArray.java
+++ src/main/java/org/nikolavp/datastructures/arrays/DynamicArray.java
@@ -1,7 +1,41 @@
 package org.nikolavp.datastructures.arrays;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class DynamicArray {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int Q = scanner.nextInt();
+        int lastAns = 0;
+
+        List[] seqList = new ArrayList[N];
+        for (int i = 0; i < N; i++) {
+            seqList[i] = new ArrayList<Integer>();
+        }
+        for (int i = 0; i < Q; i++) {
+            int q = scanner.nextInt();
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+            List<Integer> seq = findSeq(seqList, x, lastAns);
+            switch (q) {
+                case 1:
+                    seq.add(y);
+                    break;
+                case 2:
+                    lastAns = seq.get(y % seq.size());
+                    System.out.println(lastAns);
+                    break;
+                default:
+                    throw new IllegalStateException("Invalid query type " + q);
+            }
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private static List<Integer> findSeq(List[] seqList, int x, int lastAns) {
+        return seqList[(x ^ lastAns) % seqList.length];
     }
 }
