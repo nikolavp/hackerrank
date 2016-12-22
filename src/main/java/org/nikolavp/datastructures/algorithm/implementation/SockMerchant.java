294517bac13f53f377e8bb313338be40bc7f5799
Nikola Petrov <nikolavp@gmail.com>
2016-12-22 12:53:57 +0200

    Sock merchant challenge

--- src/main/java/org/nikolavp/datastructures/algorithm/implementation/SockMerchant.java
+++ src/main/java/org/nikolavp/datastructures/algorithm/implementation/SockMerchant.java
@@ -1,4 +1,22 @@
 package org.nikolavp.datastructures.algorithm.implementation;
 
+import java.util.Scanner;
+
 public class SockMerchant {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int c[] = new int[101];
+        for (int i = 0; i < n; i++) {
+            int socketColor = in.nextInt();
+            c[socketColor]++;
+        }
+
+        int sockPairs = 0;
+        for (int i = 0; i < c.length; i++) {
+            sockPairs += c[i] / 2;
+        }
+
+        System.out.println(sockPairs);
+    }
 }
