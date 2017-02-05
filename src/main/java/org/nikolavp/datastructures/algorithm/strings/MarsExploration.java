7225787a301db52c873e2be9eb7b9e48f3714e4f
Nikola Petrov <nikolavp@gmail.com>
2017-02-06 00:05:10 +0200

    Mars exploration

---
+++ src/main/java/org/nikolavp/datastructures/algorithm/strings/MarsExploration.java
@@ -0,0 +1,20 @@
+package org.nikolavp.datastructures.algorithm.strings;
+
+import java.util.Scanner;
+
+/**
+ * https://www.hackerrank.com/challenges/mars-exploration
+ */
+public class MarsExploration {
+    public static void main(String[] args) {
+        String s = new Scanner(System.in).next();
+        char[] expected = new char[] {'S', 'O', 'S'};
+        int changed = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != expected[i % 3]) {
+                changed++;
+            }
+        }
+        System.out.println(changed);
+    }
+}
